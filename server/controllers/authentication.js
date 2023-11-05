238d864e86ecb3aba08d4aece74918f835b1274a
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-11-05 10:40:00 +0300

    JWT

---
+++ server/controllers/authentication.js
@@ -0,0 +1,41 @@
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const config = require('../config.js');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.sign({ sub: user.id, iat: timestamp }, config.scret);
+}
+
+exports.signup = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You most provide email and password.' });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+    });
+
+    user.save();
+
+    res.status(200).json({ token: tokenForUser(user) });
+  } catch (error) {
+    return next(error);
+  }
+};
--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication.js');
+
 module.exports = (app) => {
-  app.get('/', (req, res, next) => {
-    res.send(['waterbottle', 'phone', 'paper']);
-  });
+  app.post('/signup', Authentication.signup);
 };
